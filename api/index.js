8238d86cb9c3ace5fe06ea8dc93844f5295b7de8
Sakshi Sharma <83204772+si-eng@users.noreply.github.com>
2023-07-06 09:52:33 +0530

    new

---
+++ api/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  console.log('working fine on production api')
+  res.send('testing staging api micro service ok woking fine still roll back');
+>>>>>>> dev
+});
+
+const port = process.env.PORT || 3000; 
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
